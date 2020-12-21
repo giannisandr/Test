9609bb05fc7c4e5fcbbd4ae6eec61c0cd7bcbe2e
Ioannis Andreadakis <ioannis.andreadakis@ibm.com>
2020-12-21 11:09:31 +0200

    New App

--- Server.js
+++ Server.js
@@ -1,68 +1,31 @@
-var express = require('express'),
-    http = require('http'), 
-    request = require('request'),
-    bodyParser = require('body-parser'),
-    app = express();
-	
-var https = require('https');
-var fs = require('fs');
- 
-	
-var logFmt = require("logfmt");
+// load the things we need
+var express = require('express');
+var app = express();
 
-app.use(express.static(__dirname + '/client')); 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.json());  
+// use res.render to load up an ejs view file
 
-app.set('port', process.env.PORT || 8080);
+// index page 
+app.get('/', function(req, res) {
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
 
-/*Allow CORS*/
-app.use(function(req, res, next) {
-	 
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-	res.setHeader('Access-Control-Max-Age', '1000');
-	  
-	next();
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
-
-
-app.all('/proxy',  function(req, res, next) { 
-    
-    var url = req.header('SalesforceProxy-Endpoint');  
-    request({ url: url, method: req.method, json: req.body, 
-                    headers: {'Authorization': req.header('X-Authorization'), 'Content-Type' : 'application/json'}, body:req.body }).pipe(res);    
-    
-});
- 
-app.get('/' ,  function(req,res,next) {
-    res.sendfile('views/index.html');
-} ); 
-
-app.get('/index*' ,  function(req,res,next) {
-    res.sendfile('views/index.html');
-} );  
- 
-app.get('/oauthcallback.html' ,  function(req,res,next) {
-    res.sendfile('views/oauthcallback.html');
-} ); 
-
-app.get('/Main*' ,   function(req,res,next) {
-    res.sendfile('views/Main.html');
-} );
- 
-
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
-	var options = {
-      key: fs.readFileSync('./key.pem', 'utf8'),
-      cert: fs.readFileSync('./server.crt', 'utf8')
-   };
-   
-	https.createServer(options, app).listen(8081);
-	console.log("Server listening for HTTPS connections on port ", 8081);
+app.listen(8080);
+console.log('8080 is the magic port');
