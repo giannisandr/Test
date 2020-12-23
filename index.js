b2d2d0715df11e1757cbbe16ea134370d952f555
Ioannis Andreadakis <ioannis.andreadakis@ibm.com>
2020-12-23 17:21:28 +0200

    New App

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+console.log()
+
+express()
+/*Allow CORS*/
+  .use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    
+    next();
+  })
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
