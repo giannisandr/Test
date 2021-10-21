2b783496762eb90aa486af865bfe0bf0c99570b1
Andreadakis Giannis <ioannis.andreadakis1@ibm.com>
2021-10-21 19:46:07 +0300

    claim4

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ express()
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
     res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader("X-Content-Type-Options","nosniff"); 
+	res.setHeader("Referrer-Policy","strict-origin-when-cross-origin"); 
     res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
     res.setHeader('Access-Control-Allow-Credentials', true);
